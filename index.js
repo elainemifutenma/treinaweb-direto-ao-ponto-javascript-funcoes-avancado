b81b9e16d177ae0c275c8cc7009d734754ed35b7
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-04-10 15:47:23 -0300

    01:04 - Função auto executável

--- index.js
+++ index.js
@@ -1,64 +1,14 @@
 
-// function soma(a, b) {
-//     console.log(a + b);
-
-// }
-
-// const resultado = soma(5, 10);
-
-// console.log(resultado);
-
-// ** Retornando um valor simples: **
-// function soma(a, b) {
-//     console.log(a + b);
-//     return a + b;
-// }
-
-// const resultado = soma(5, 10);
-
-// console.log(resultado);
+/*
+    funções auto executáveis
+    (também conhecidas como IIFEs - Immediately Invoked Function Expressions)
+*/
 
 
+(async (idade) => {
+    let nome = "Ariel";
+    console.log("Olá ", nome, "Sua idade é: ", idade);
 
-// ** Retornando um arrays **
-
-// function criarPessoa(nome, idade) {
-//     return [nome, idade]
-// }
-
-// const [nome, idade] = criarPessoa('Ariel', 29);
-
-// console.log(nome, idade);
-
-
-
-// ** Retornando um objeto **
-
-
-// function criarPessoa(nome, idade) {
-//     return {
-//         nome,
-//         idade
-//     }
-// }
-
-// const { nome, idade } = criarPessoa('Ariel', 29);
-
-// console.log(nome, idade);
-
-
-
-// ** Retornando uma função: **
-
-function criarPessoa(nome, idade) {
-
-    function myFunc() {
-        return `${nome}  ${idade}`;
-    }
-
-    return myFunc()
-}
-
-const myFunc = criarPessoa('Ariel', 29);
-
-console.log(myFunc);
+    const resultado = await Promise.resolve('promise resolvida');
+    console.log("Executou: ", resultado);
+})(29)
