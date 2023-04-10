f25726f8bec3c015eb6914b68a792e8dbd59413c
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-04-10 15:48:55 -0300

    01:08 - Funções de ordem superior

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 
-function main(callback) {
-    setTimeout(callback, 2000);
+/*
+    Uma função de ordem superior, 
+    ou higher order function (HOF),
+*/
 
+function dobro(n) {
+    return n * 2;
 }
 
-main(() => console.log("executou"))
+function triplo(n) {
+    return n * 3;
+}
+
+function aplicaFuncao(funcao, n) {
+    return funcao(n);
+}
+
+console.log(aplicaFuncao(dobro, 5));
+
+console.log(aplicaFuncao(triplo, 5));
