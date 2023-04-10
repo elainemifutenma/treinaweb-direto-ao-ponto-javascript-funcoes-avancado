7c293ed5c74c815c0909f9c68b1f35af1a3699c5
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-04-10 15:49:53 -0300

    01:11 - Função geradora

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-let i = 0;
-function main() {
-    console.log("Executou main: ", ++i);
-    if (i === 1000) {
-        return;
+
+//  função geradora(ou generator function)
+
+
+function* gerador() {
+    let i = 0
+    while (i < 5) {
+        yield i;
+        i++;
     }
-    main();
 }
 
-main();
+const g = gerador();
+
+for (let index = 0; index < 10; index++) {
+    console.log(g.next());
+}
