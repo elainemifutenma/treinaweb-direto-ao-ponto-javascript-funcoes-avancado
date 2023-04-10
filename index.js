e89138764591ea0c7b0df3661cae5f31e8ee9803
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-04-10 15:46:34 -0300

    01:02 - Parâmetros da função

---
+++ index.js
@@ -0,0 +1,57 @@
+
+// ** pâmetro posicional **
+
+// function calculador(valorX, valorY) {
+//     console.log(valorX + valorY);
+// }
+
+// calculador(5, 10)
+
+
+
+// ** parâmetros com valor default **
+// function calculador(valorX, valorX) {
+//     console.log(valorX, valorX);
+//     console.log(valorX + valorX);
+// }
+
+// calculador(5, 7)
+
+
+
+// ** atribuição por desestruturação **
+
+// const { nome, idade } = {
+//     nome: 'ariel',
+//     idade: 25,
+// }
+// console.log(nome, idade);
+// const [nome,] = ["Ariel", 29];
+
+// function printUser({ nome, idade }) {
+//     console.log(nome, idade);
+// }
+
+// printUser({
+//     nome: 'ariel',
+//     idade: 25,
+// });
+
+
+// ** atribuição por rest operator **
+
+// function printUser({ nome, idade, ...props }) {
+//     console.log(nome, idade, props);
+// }
+
+function printUser() {
+    console.log(arguments);
+}
+
+printUser({
+    nome: 'ariel',
+    idade: 25,
+    cor: 'P',
+    sexo: 'M'
+}, 'segundo argumento');
+
