d7bc73c9bf5742b3cd1ccebeec92b5dead0a100d
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-04-10 15:47:49 -0300

    01:05 - Currying

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 
-/*
-    funções auto executáveis
-    (também conhecidas como IIFEs - Immediately Invoked Function Expressions)
-*/
+function multiplicador(a) {
+    return function (b) {
+        return a * b;
+    };
+}
 
 
-(async (idade) => {
-    let nome = "Ariel";
-    console.log("Olá ", nome, "Sua idade é: ", idade);
-
-    const resultado = await Promise.resolve('promise resolvida');
-    console.log("Executou: ", resultado);
-})(29)
+console.log(multiplicador(5)(10));
